68f914393ff5ddfad877c042f073ed5f85b0b54b
galileofinch <galileofinchdudi@gmail.com>
2019-02-17 15:43:22 +0800

    chapter3

---
+++ c3/demo1.js
@@ -0,0 +1,37 @@
+'use strict'
+
+require('@babel/register')
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+ 
+// let [a, b, c] = [1, 2, 3];
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// let [foo, [[bar], baz]] = [1, [[2], 3]];
+
+// let [ , , baz] = ["foo", "bar", "baz"]
+
+// let [foo, , baz] = [1, 2, 3];
+
+// let [head, ...tail] = [1, 2, 3, 4];
+
+// let [x, y, ...z] = ['a'];
+
+// let [x, y] = [1, 2, 3];
+
+// let [x, [y], z] = [1, [2, 3], 4];
+
+// let [foo] = 1;
+// let [foo] = false;
+// let [foo] = NaN;
+// let [foo] = undefined;
+// let [foo] = null;
+// let [foo] = {};
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
